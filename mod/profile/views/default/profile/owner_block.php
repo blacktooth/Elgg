c9d00724537c69663a417b096efb2c1d0cbc8935
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2011-07-10 02:46:40 +0530

    profile-action-menu items are more controllable now

--- mod/profile/views/default/profile/owner_block.php
+++ mod/profile/views/default/profile/owner_block.php
@@ -24,7 +24,7 @@ $profile_actions = '';
 if (elgg_is_logged_in() && $actions) {
 	$profile_actions = '<ul class="elgg-menu profile-action-menu mvm">';
 	foreach ($actions as $action) {
-		$profile_actions .= '<li>' . $action->getContent(array('class' => 'elgg-button elgg-button-action')) . '</li>';
+		$profile_actions .= '<li>' . $action->getContent(array('class' => 'elgg-button elgg-button-action elgg-button-action-' . $action->getName())) . '</li>';
 	}
 	$profile_actions .= '</ul>';
 }
